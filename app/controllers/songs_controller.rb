f2da019b4bb214861f4b401b58cdbdaf34fcec59
5-4 <github email address>
2018-01-03 16:52:06 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < Sinatra::Base
+  use Rack::Flash
+  enable :sessions
   set :views, Proc.new { File.join(root, "../views/songs/") }
 
   get '/songs' do
@@ -17,6 +21,7 @@ class SongsController < Sinatra::Base
   		Genre.find_or_create_by(name: genre)
   	end
   	song = artist.songs.create(song_params)
+  	flash[:message] = "Successfully created song."
   	redirect "/songs/#{song.slug}"
   end
 
@@ -24,5 +29,22 @@ class SongsController < Sinatra::Base
   	@song = Song.find_by_slug(params[:song])
   	erb :show
   end
+
+  get '/songs/:song/edit' do
+  	@song = Song.find_by_slug(params[:song])
+  	erb :edit
+  end
+
+  post '/songs/:song' do
+  	updates = { artist: Artist.find_or_create_by(params[:artist]) }
+  	updates[:genres] = params[:genres].collect do |genre|
+  		Genre.find_or_create_by(name: genre)
+  	end
+
+  	song = Song.find_by_slug(params[:song])
+  	song.update(updates)
+  	flash[:message] = "Successfully updated song."
+  	redirect "/songs/#{song.slug}"
+	end  	
 
 end
